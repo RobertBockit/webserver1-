ce45e329bf3adecb3009af0707397aea3ebb81b1
CodeFanaticLV <robertterekhovich@gmail.com>
2024-01-16 10:04:54 +0100

    test1

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { users } = require('./database');
+const database = {users : users};
+console.log(users);
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.post("/api/login", function (req,res) {
 
-
-app.post("/api/login", function (req,res){
     let username = req.body.username;
     let password = req.body.password;
 
-    if(username === "test" && password === "admin") {
-        res.send('Hello World!' + username + password)
-    }else{
-        res.send('Login not authorized')
+    for (let i = 0; i < database.users.length; i++) {
+        const userToCheck = users[i];
+        console.log("Checking User Number " + i)
+        if (userToCheck.username === username && userToCheck.password === password) {
+
+            res.sendStatus(200);
+            console.log("User '" + username + "' is logged in")
+            break;
+        }
+        res.sendStatus(401)
+
+
+
+
     }
-
-
-
 })
 
 
---
+++ database.js
@@ -0,0 +1,14 @@
+const users = [{
+    username: "test",
+    password : "admin"
+    },
+    {
+        username: "test1",
+        password : "admin1"
+    },
+    {
+        username: "test2",
+        password : "admin2"
+    }]
+
+module.exports = {users};
